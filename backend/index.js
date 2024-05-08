70d6eaaa88fe882568f85feb0ba39eb0fb828f5e
RecFanJia <geo.fanjia@gmail.com>
2024-05-08 23:56:08 +0200

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -20,30 +20,6 @@ morgan.token('body', function (req, res) {
 });
 
 
-
-//let persons = [
-//    {
-//        "id": 1,
-//        "name": "Arto Hellas",
-//        "number": "040-123456"
-//      },
-//      {
-//        "id": 2,
-//        "name": "Ada Lovelace",
-//        "number": "39-44-5323523"
-//      },
-//      {
-//        "id": 3,
-//        "name": "Dan Abramov",
-//        "number": "12-43-234345"
-//      },
-//      {
-//        "id": 4,
-//        "name": "Mary Poppendieck",
-//        "number": "39-23-6423122"
-//      }
-//]
-
 //app.get('/info', (request, response) => {
 //    response.send(   
 //     `Phonebook has info for ${persons.length} people<br/>
@@ -58,26 +34,27 @@ app.get('/api/persons', (request, response) => {
 })
 
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
+app.get('/api/persons/:_id', (request, response) => {
+  Person.findById(request.params._id).then(person => {
     response.json(person)
   })
   })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    Person = Person.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:_id', (request, response) => {
+    Person.findByIdAndDelete(request.params._id)
+    .then(result => {
+      if (result) {
+        response.status(204).end();
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 })
-
-
-    
-
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  const newId = Math.random(9999999)
-
+  
   if (body.name===undefined) {
     return response.status(400).json({
       error: 'name missing'
@@ -97,7 +74,6 @@ app.post('/api/persons', (request, response) => {
   //  }
 
 const person = new Person({
-  id: newId,
   name: body.name,
   number: body.number 
 })
--- backend/models/person.js
+++ backend/models/person.js
@@ -15,17 +15,17 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  Name: String,
-  Number: String,
+  name: String,
+  number: String,
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 module.exports = mongoose.model('Person', personSchema)
 
