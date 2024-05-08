1d8d74ad5de083d3ad00745a2b41158f99fd104a
RecFanJia <geo.fanjia@gmail.com>
2024-05-08 11:44:50 +0200

    finished part3 3.12

--- backend/README.md
+++ backend/README.md
@@ -3,3 +3,6 @@ MongoDB
 Username:geofanjia
 Password:ZGibq5HsvWEXjerK
 Connection string: mongodb+srv://geofanjia:<password>@cluster0.dw4tvqm.mongodb.net/
+                    mongodb+srv://geofanjia:ZGibq5HsvWEXjerK@cluster0.dw4tvqm.mongodb.net/
+cmd: node mongo.js <password>
+    node mongo.js ZGibq5HsvWEXjerK
--- backend/mongo.js
+++ backend/mongo.js
@@ -15,19 +15,35 @@ mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  Name: String,
+  Number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
+const person = new Person({
+Name: process.argv[3],
+Number: process.argv[4],
 })
 
-note.save().then(result => {
-  console.log('note saved!')
+// if input has no name and number, then display all the persons in the phonebook 
+if(process.argv.length<5){
+  Person.find({}).then(result => {
+    console.log("phonebook:")
+    result.forEach(person => {
+      console.log(
+        ` ${person.Name} ${person.Number}`)
+    })
+    mongoose.connection.close()
+  })
+}else{
+// if input has name and number, then and them to phonebook
+  person.save().then(result => {
+  console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
   mongoose.connection.close()
-})
+}).catch(err => {
+  console.error("error", err);
+});
+}
+
