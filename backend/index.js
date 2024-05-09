f30deddda1e4deedf50402b3d29115113fd2355d
RecFanJia <geo.fanjia@gmail.com>
2024-05-09 11:39:33 +0200

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -64,7 +64,7 @@ app.delete('/api/persons/:_id', (request, response) => {
     .catch(error => next(error));
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   
   if (body.name===undefined) {
@@ -93,6 +93,7 @@ const person = new Person({
 person.save().then(savedPerson => {
   response.json(savedPerson)
 })
+.catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -115,7 +116,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
--- backend/models/person.js
+++ backend/models/person.js
@@ -15,8 +15,21 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /\b\d{2,3}-\d+\b/.test(v) && v.length >= 8;
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  },
 })
 
 personSchema.set('toJSON', {
